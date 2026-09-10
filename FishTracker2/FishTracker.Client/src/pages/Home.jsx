c4b0495170375ba256989dd6c52244a18a6fa3ae
keviche <kche9533@gmail.com>
2026-09-09 23:32:51 -0400

    redesigned homepage to focus on aquarium ui and better support swipe navigation

--- FishTracker2/FishTracker.Client/src/pages/Home.jsx
+++ FishTracker2/FishTracker.Client/src/pages/Home.jsx
@@ -1,30 +1,29 @@
 import './Home.css'
+
 function Home() {
     return (
-        <main className = 'home'>
-            <section className = 'top'>
-                <div className = 'title'>FishTracker</div>
+        <main className="home">
+
+            <header className="home-header">
+                <h1>FishTracker</h1>
+            </header>
+
+            <section className="aquarium">
+                <div className="aquarium-water">
+                    <div className="fish fish1">🐟</div>
+                    <div className="fish fish2">🐠</div>
+                    <div className="fish fish3">🐟</div>
+                </div>
             </section>
 
-
-            <section className = 'bottom'>
-                <div className='description'>Ready to begin your fishing journey goyim?</div>
-
-                <nav className = 'buttons'>
-                    <button>Home</button>
-                    <button>Aquarium</button>
-                    <button>My Gear</button>
-                    <button>Leaderboard</button>
-                    <button>Profile</button>
-                    <button>Settings</button>
-                </nav>
-
+            <section className="bottom">
+                <div className="description">
+                    Ready to begin your fishing journey?
+                </div>
             </section>
-            
+
         </main>
-
-
-
-    );
+    )
 }
+
 export default Home
